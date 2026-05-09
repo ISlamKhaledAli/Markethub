fbafb7673767af6dff79571114cb17ebac7d9c19
mostafamohamedsalah25 <mostafamohamedsalah25@gmail.com>
2026-05-09 12:57:00 +0300

    feat(core): implement robust auth architecture and JWT interceptors

--- frontend/src/app/core/guards/role-guard.ts
+++ frontend/src/app/core/guards/role-guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth';
 
-export const roleGuard: CanActivateFn = (route, state) => {
-  return true;
+export const roleGuard = (allowedRoles: string[]) : CanActivateFn => {
+  return (route, state) => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+    const user = authService.currentUser();
+
+    if (authService.isAuthenticated() && user && allowedRoles.includes(user.role)) {
+      return true;
+    }
+
+    router.navigate(['/']);
+    return false;
+  };
 };
